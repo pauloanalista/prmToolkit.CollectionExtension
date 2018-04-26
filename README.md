d4ddacb29397e21287254cb57e25e28b4e16047c
Paulo Rogério Martins Marques <paulo.analista@outlook.com>
2018-04-26 15:07:36 -0300

    Publicando

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# prmToolkit.CollectionExtension
+# prmToolkit
+
+# CollectionExtension
 Classe responsável por estender as funcionalidades de uma coleção de objetos.
+
+### Installation - CollectionExtension
+
+Para instalar, abra o prompt de comando Package Manager Console do seu Visual Studio e digite o comando abaixo:
+
+Para adicionar somente a referencia da dll
+```sh
+Install-Package prmToolkit.CollectionExtension
+```
+### Recursos disponíveis
+- SplitCollection (Divide uma coleção em coleções menores, baseado em uma quantidade de registros informada)
+
+
+### Alguns exemplos
+```sh
+//Divide uma grande coleção em X coleções de 100 registros
+myCollection.SplitCollection(100).ToList().ForEach( mySplitCollection =>
+{
+	//Loop com apenas 100 registros
+	mySplitCollection.ToList().ForEach(x =>
+		{
+			//Executa alguma ação
+		});
+});
+```        
---
+++ prmToolkit.CollectionExtension/CollectionExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+
+namespace prmToolkit.CollectionExtension
+{
+    public static class CollectionExtension
+    {
+        public static IEnumerable<IEnumerable<T>> SplitCollection<T>(this IEnumerable<T> source, int recordCount)
+        {
+            if (source == null || recordCount == 0)
+            {
+                throw new Exception("É obrigatório passar uma coleção e definir a quantidade de registros em que deseja paginar.");
+            }
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    var currentPage = new List<T>(recordCount){
+                        enumerator.Current
+                    };
+
+                    while (currentPage.Count < recordCount && enumerator.MoveNext())
+                    {
+                        currentPage.Add(enumerator.Current);
+                    }
+                    yield return new ReadOnlyCollection<T>(currentPage);
+                }
+            }
+        }
+    }
+}
